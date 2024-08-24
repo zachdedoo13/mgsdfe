e8a820d8196fcfa360af2ad0297ffdfdd218a0bf
zacha <zachary.campbell2006@outlook.com>
2024-08-24 21:39:42 +1000

    pkg

---
+++ pkg/sdt_thing_bg.wasm.d.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_webhandle_free(a: number, b: number): void;
+export function webhandle_new(): number;
+export function webhandle_start(a: number, b: number, c: number): number;
+export function webhandle_destroy(a: number): void;
+export function webhandle_has_panicked(a: number): number;
+export function webhandle_panic_message(a: number, b: number): void;
+export function webhandle_panic_callstack(a: number, b: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h74b1f819f7234047(a: number, b: number, c: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h9da8298128974c38(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h7643d960fd7beb05(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h2ec1cfc9dbe1d356(a: number, b: number, c: number): void;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h0b684dfffed5d693(a: number, b: number, c: number, d: number): void;
