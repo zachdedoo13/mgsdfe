5269e9c9d83f4a11a557d228431dc732ca074e31
zacha <zachary.campbell2006@outlook.com>
2024-09-14 01:00:50 +1000

    cleanup

--- common/src/singletons/settings.rs
+++ common/src/singletons/settings.rs
@@ -3,13 +3,20 @@ use crate::init_none_static;
 
 init_none_static!(SETTINGS: Settings);
 
-
+/// global settings for the app
 pub struct Settings {}
 impl Settings {
    pub fn new(_ctx: &Context) -> Self {
+      // load self from persistent storage
       Self {}
    }
 
    pub fn test(&self) {}
+}
+
+impl Default for Settings {
+   fn default() -> Self {
+      Self {}
+   }
 }
 
